09f44279348cface665f34cad318c8c4eda8207a
Andrei Rudenko <rudenkopaint@gmail.com>
2023-04-28 00:16:23 +0300

    Fixed missing khafile.js

---
+++ khafile.js
@@ -0,0 +1,17 @@
+let p = new Project("nuc");
+p.addSources("./Sources");
+
+p.addLibrary("nuclib"); 
+p.addShaders("./Sources/Shaders/**");
+
+p.addAssets(
+    "Assets/**", 
+    {
+        nameBaseDir: "assets", 
+        destination: "assets/{dir}/{name}", 
+        name: "{dir}/{name}", 
+        noprocessing: true, 
+        notinlist: true
+    }
+);
+resolve(p);
